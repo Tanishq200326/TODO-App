aaf62a7aa61eec3149fda701d2494ec905f8a644
9301680770 <tanishqprajapati84@gmail.com>
2024-07-11 12:56:40 +0530

    Deleted requirements.txt

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login as loginUser, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from app.forms import TODOForm
